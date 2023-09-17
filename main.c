21846b95b2240589e39db7a42e42de9f5bbc8786
N1ckName192 <nikita.kolomyts@yandex.ru>
2023-09-17 19:35:56 +0300

    Update commit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main() {
     int requests;
+    printf("Введите количество наборов данных:");
     scanf("%d", &requests);
     for(int i=1; i<=requests; i++) {
         long long liters, first_cost, second_cost;
@@ -9,7 +10,7 @@ int main() {
         if (liters % 2 == 0 && liters >= 2 && 2*first_cost >= second_cost) {
             printf("%lld\n", (liters/2)*second_cost);
         } else if (liters % 2 != 0 && liters >= 2 && 2*first_cost >= second_cost){
-            printf("%lld\n", (liters/2)*second_cost + first_cost);
+            printf("Минимальная стоимость для покупки %lld литров воды при цене %lld рублей за бутылку первого типа и %lld рублей за бутылку второго типа составляет %lld рублей.\n", liters, first_cost, second_cost, (liters/2)*second_cost + first_cost);
         } else {
             printf("%lld\n", liters * first_cost );
         }
